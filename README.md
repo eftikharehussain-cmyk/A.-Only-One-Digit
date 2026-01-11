87be708b225b686f0936b8d1baf41e462f40ea86
Mohammad hussain <eftikharehussain@gmail.com>
2026-01-11 16:49:23 +0430

    Add Python script to find minimum digit

--- README.md
+++
@@ -1,1 +0,0 @@
-# A.-Only-One-Digit
---
+++ README.py
@@ -0,0 +1,7 @@
+# A.-Only-One-Digit
+for _ in range(int(input())):
+    x = int(input())
+    s = str(x)
+    lst = list(s)
+    _min = min(lst)
+    print(_min)
